8d7bb4577f3dda22950dc7711a9bfd6e516c1c60
Jack Machin <jack.machin125@gmail.com>
2016-10-06 10:02:12 +0100

    Update header.php
    Google Analytics stuff

--- header.php
+++ header.php
@@ -53,24 +53,6 @@
 
 <?php flush(); ?>
 
-<!-- Google Analytics -->
-<script type="text/javascript">
-
-            var allowed = $.cookie("warnrsallowed");
-            if (allowed == null) {
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-20034801-8']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-            }
-
-
-</script>
 </head>
 
 <body>
